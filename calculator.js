e1e18b45d5e95ba617aa72251145658ae14a58da
aveshkhan726 <79111094+aveshkhan726@users.noreply.github.com>
2021-02-16 01:08:00 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,40 @@
+
+
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+
+  }
+
+ 
+
+  function double(x) {
+
+    x=form.displayResult.value;
+    var y=x*x;
+
+    form.displayResult.value=y;
+  } 
+
+
+  function squareRoot(x) {
+
+    x=form.displayResult.value;
+   var y= Math.sqrt(x);
+
+    form.displayResult.value=y;
+  } 
+
+
+
+
+  function percentage(x) {
+
+    x=form.displayResult.value;
+   var y= (x/100)
+
+    form.displayResult.value=y;
+  } 
+
+
+  
+ 
